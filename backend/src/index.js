ff06aa1dd50869a33eb74f8f3b0ed143f6649db8
Lucas8x <lucas.poring99@gmail.com>
2020-01-14 20:13:32 -0300

    Init

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const routes = require('./routes')
+const app = express()
+require('dotenv').config()
+
+const port = process.env.PORT || 3001
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB")
+}).catch((err) => {
+  console.log(`DB Connection Error: ${err}`)
+})
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(routes)
+
+app.listen(port, () => {
+  console.log(`Server listen port: ${port}`)
+})
